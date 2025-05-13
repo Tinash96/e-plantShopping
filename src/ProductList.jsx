f6aae1dde7fd0519d4cd4d0f48154cb9186f741d
Tinash96 <shivititm@gmail.com>
2025-05-13 04:55:22 -0400

    Refactoring

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
